6f5abccddf1318a75c2e7e7ca95ce06a259d8034
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-25 08:41:53 -0500

    modify: Adding methods to creates instances in Config class

--- src/app/Config.java
+++ src/app/Config.java
@@ -1,4 +1,28 @@
 package app;
 
+import app.controllers.Controller;
+import app.controllers.PokemonController;
+import app.entities.Pokemon;
+import app.repositories.PokemonRepository;
+import app.repositories.Repository;
+import app.services.PokemonService;
+import app.services.Service;
+
+/**
+ * Creates the instances for the dependencies in every component of code
+ * @author  Gerardo Aguilar
+ * @version 1.0.0 25/08/2021.
+ */
 public class Config {
+    public static Repository<Pokemon, Long> repository() {
+        return new PokemonRepository();
+    }
+
+    public static Service service () {
+        return new PokemonService(Config.repository());
+    }
+
+    public static Controller controller () {
+        return new PokemonController(Config.service());
+    }
 }
